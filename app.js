af2fd21366728b6f2ac1f1be130f05d989f97b72
Honey Patel <you@example.com>
2022-03-31 23:39:23 +0530

    first commit

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 // initialize our express app
 const app = express();
-let port = 4001;
+var db = process.env.MONGO_URL
+var port = process.env.PORT || 4001;
 const student = require('./routes/studentr'); // Imports routes for the products
 const mongoose = require('mongoose');
-let dburl = 'mongodb+srv://Honey096:gubrGr0ZV6PqcqN1@cluster0.pcthb.mongodb.net/srms?retryWrites=true&w=majority';
-const mongoDB = process.env.MONGODB_URI || dburl;
-mongoose.connect(mongoDB);
+const connectDB = require('database/connection')
+connectDB();
 mongoose.Promise = global.Promise;
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use('/student', student);
 
 app.listen(port, () => {
-    console.log('Server is up and running on port numner ' + port);
+    console.log('Server is up and running on port number ' + port);
 });
