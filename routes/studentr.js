4506034fd554f5c7fc7883a53f1b38f947bab45e
Honey Patel <you@example.com>
2022-04-01 08:03:27 +0530

    first commit

--- routes/studentr.js
+++ routes/studentr.js
@@ -7,4 +7,5 @@ const student_controller = require('../controllers/studentc');
 
 // a simple test url to check that all of our files are communicating correctly.
 router.post('/create', student_controller.student_create);
+router.get('/:id', student_controller.student_details);
 module.exports=router;
