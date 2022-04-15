58376d635a0a2ce093bcff0b900f2e1ab97b2ac2
Honey Patel <you@example.com>
2022-04-16 00:52:12 +0530

    commit

--- server/models/subject.js
+++ server/models/subject.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 const SubjectSchema = new Schema({
   name: { type: String, required: true  },
   sem: { type: Schema.Types.ObjectId, ref: "semester" },
-  branch: { type: Schema.Types.ObjectId, ref: "student" }
+  branch: { type: Schema.Types.ObjectId, ref: "branch" }
  
 });
 
