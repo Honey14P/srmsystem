7e59a9a79399f519a2143e02b60778dfe93e9ae7
Honey Patel <you@example.com>
2022-04-16 08:48:13 +0530

    comiited

--- app.js
+++ app.js
@@ -30,9 +30,9 @@ app.use(morgan('tiny'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/css', express.static(path.resolve(__dirname, "public/css")))
-app.use('/img', express.static(path.resolve(__dirname, "public/img")))
-app.use('/js', express.static(path.resolve(__dirname, "public/js")))
+//app.use('/css', express.static(path.resolve(__dirname, "public/css")))
+app.use('/img', express.static(path.resolve(__dirname, 'public/img')));
+app.use('/js', express.static(path.resolve(__dirname, 'public/js')))
 
 //app.use('/admin', admin);
 //app.use('/student',student);
