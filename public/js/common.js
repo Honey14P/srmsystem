5551b54724a19e5d2c19069bc6745242eb221c66
Honey Patel <you@example.com>
2022-04-16 08:48:04 +0530

    commited

---
+++ public/js/common.js
@@ -0,0 +1,7 @@
+Userdb.find()
+            .then(user => {
+                res.send(user)
+            })
+            .catch(err => {
+                res.status(500).send({ message : err.message || "Error Occurred while retriving user information" })
+            })
--- server/routes/home.js
+++ server/routes/home.js
@@ -6,6 +6,7 @@ const express = require('express');
 //const jsonwt = require("jsonwebtoken");
 const route = express.Router()
 var User = require("../models/student");
+const router = require('./admin/studentr');
 
 
 
@@ -24,12 +25,55 @@ route.get('/signup', (req, res) => {
     res.render('signup');  
 
 });
+route.get('/admin', (req, res) => {
+    
+    res.render('adminhome', {
+        errorMessage: "suceess",
+      });  
 
+});
+route.get('/admin/addnewstudent', (req, res) => {
+    
+    res.render('addnewstudent');  
+
+});
+route.get('/admin/managestudent', (req, res)=>{
+
+    if(req.query.id){
+        const id = req.query.id;
+
+        User.findById(id)
+            .then(data =>{
+                if(!data){
+                    res.status(404).send({ message : "Not found user with id "+ id})
+                }else{
+                    
+                    res.send(data)
+                }
+            })
+            .catch(err =>{
+                res.status(500).send({ message: "Erro retrieving user with id " + id})
+            })
+
+    }else{
+        User.find()
+            .then(newUser => {
+               
+      res.render('managestudent', { posts: newUser });
+            })
+            .catch(err => {
+                res.status(500).send({ message : err.message || "Error Occurred while retriving user information" })
+            })
+    }
+
+    
+});
 route.post("/signup",  (req, res) => {
     var newUser = new User({
       name: req.body.name,
       fname: req.body.fname,
       email: req.body.email,
+      rollno: req.body.rollno,
       password: req.body.password,
       gender:req.body.gender,
       phone:req.body.phone,
@@ -58,6 +102,36 @@ route.post("/signup",  (req, res) => {
     
     res.render('studenthome');  
 
+});
+route.get('/find', (req, res)=>{
+
+    if(req.query.id){
+        const id = req.query.id;
+
+        User.findById(id)
+            .then(data =>{
+                if(!data){
+                    res.status(404).send({ message : "Not found user with id "+ id})
+                }else{
+                    
+                    res.send(data)
+                }
+            })
+            .catch(err =>{
+                res.status(500).send({ message: "Erro retrieving user with id " + id})
+            })
+
+    }else{
+        User.find()
+            .then(newUser => {
+                res.send(newUser)
+            })
+            .catch(err => {
+                res.status(500).send({ message : err.message || "Error Occurred while retriving user information" })
+            })
+    }
+
+    
 });
 
 module.exports=route;
