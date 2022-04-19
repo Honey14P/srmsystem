49c256539af578527fb9b44edd35de05c3b6b07f
arjun <202112038@daiict.ac.in>
2022-04-19 19:06:55 +0530

    commit

--- server/models/student.js
+++ server/models/student.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 const Schema = require('mongoose').Schema;
 const passportLocalMongoose = require("passport-local-mongoose");
-
+const jwt = require("jsonwebtoken");
 const studentSchema = mongoose.Schema({
     name:{
         type:String,
@@ -34,11 +35,31 @@ const studentSchema = mongoose.Schema({
         type:Number,
        
     },
-
-    sem: { type: Schema.Types.ObjectId, ref: "semester" },
-  branch: { type: Schema.Types.ObjectId, ref: "branch"}
-
-    
+    sem:{
+        type:String,
+    },
+    branch:{
+        type:String,
+    },
+    tokens:[{
+        token:{
+            type:String,
+            required:true
+        }
+    }]
+//     sem: { type: Schema.Types.ObjectId, ref: "semester" },
+//   branch: { type: Schema.Types.ObjectId, ref: "branch"}
 })
+studentSchema.methods.generateAuthToken = async function(){
+    try{
+        const token = await jwt.sign({_id:this._id.toString()},process.env.SECRET_KEY);
+        this.tokens=this.tokens.concat({token:token})
+        await this.save();
+        return token;
+    }catch(error){
+        res.send("the error part"+error);
+    }
+}
+
 studentSchema.plugin(passportLocalMongoose);
 module.exports = mongoose.model('student',studentSchema)
